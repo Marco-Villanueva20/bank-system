cbf2ab72c585669fd9b48e620dfe2e9d82ae8e01
Marco <marco_x1230@hotmail.com>
2025-09-21 01:56:10 -0500

    agregando anotaciones que ayude con la ejecucion de las querys

--- src/main/java/com/bank/system/account/repository/CustomerRepository.java
+++ src/main/java/com/bank/system/account/repository/CustomerRepository.java
@@ -2,8 +2,10 @@ package com.bank.system.account.repository;
 
 import com.bank.system.account.model.Customer;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface CustomerRepository extends ReactiveCrudRepository<Customer, Long> {
     Flux<Customer> findByType(String type);
 }
