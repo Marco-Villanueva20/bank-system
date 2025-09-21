38ad789bd3d2c6efdcc899919a4b2ff9f7851e61
Marco <marco_x1230@hotmail.com>
2025-09-21 01:50:28 -0500

    Arreglando las variables dandole el formato al nombre con @Column

--- src/main/java/com/bank/system/account/model/Customer.java
+++ src/main/java/com/bank/system/account/model/Customer.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Builder
@@ -18,10 +19,18 @@ public class Customer {
     @Id
     private Long id;
     private CustomerType type; // PERSONAL o BUSINESS //EMPRESARIAL
-    private String firstName;  // para personal
+    @Column("first_name")
+    private String firstName;
+
+    @Column("last_name")
     private String lastName;
-    private String companyName; // para empresarial
+
+    @Column("company_name")
+    private String companyName;
+
+    @Column("document_number")
     private String documentNumber;
+
     private String email;
     private String phone;
 }
